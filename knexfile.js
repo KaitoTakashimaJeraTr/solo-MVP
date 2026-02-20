25d293de60bf6921daf9e7b82f4b2e00f95eca54
KaitoTakashima <jera.hq.devtr.mem008@outlook.jp>
2026-02-20 15:56:54 +0900

    デプロイ開始&エラー対応

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,24 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.POSTGRES_HOST || "localhost",
-      user: process.env.POSTGRES_USER || "JERA_DevPC",
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB || "calorie_app",
-      port: process.env.POSTGRES_PORT || 5432,
+      database: process.env.POSTGRES_DB,
+      port: process.env.POSTGRES_PORT,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      connection: process.env.DATABASE_URL,
     },
     migrations: {
       directory: "./db/migrations",
