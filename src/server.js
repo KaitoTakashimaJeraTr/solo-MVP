82d6613a0e56b64e03c6e13b1128536eb383ba17
KaitoTakashima <jera.hq.devtr.mem008@outlook.jp>
2026-02-23 15:25:00 +0900

    server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
+const path = require("path");
 const { buildApp } = require("./app");
 
 const PORT = process.env.PORT || 3000;
 const app = buildApp();
+
+// React のビルドファイルを配信
+app.use(require("express").static(path.join(__dirname, "../front/dist")));
+
+// すべてのルートで React の index.html を返す
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
