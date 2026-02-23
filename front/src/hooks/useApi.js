42b3587d258c3a476f3e654b9f1ba409fca82374
KaitoTakashima <jera.hq.devtr.mem008@outlook.jp>
2026-02-23 14:47:11 +0900

    useApi2

--- front/src/hooks/useApi.js
+++ front/src/hooks/useApi.js
@@ -1,19 +1,17 @@
 export function useApi() {
-  const API_BASE = import.meta.env.VITE_API_URL;
+  const get = async (path) => {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api${path}`);
+    return res.json();
+  };
 
   const post = async (path, body) => {
-    const res = await fetch(`${API_BASE}/api${path}`, {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api${path}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
-    return res.json();
-  };
-
-  const get = async (path) => {
-    const res = await fetch(`${API_BASE}/api${path}`);
     return res.json();
   };
 
